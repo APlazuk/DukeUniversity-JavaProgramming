eb056898edd40db40d250d544ec5e83ee5a41b97
APlazuk <aleksandraplazuk@o2.pl>
2020-09-14 17:46:50 +0200

    week2 - assigment

--- src/main/java/week2/DebuggingPart1.java
+++ src/main/java/week2/DebuggingPart1.java
@@ -4,13 +4,12 @@ public class DebuggingPart1 {
 
     public static void findAbc(String input) {
         int index = input.indexOf("abc");
-        while (true) {
-            if (index == -1 || index >= input.length() - 3) {
-                break;
-            }
+        while (index != -1 && index < input.length() - 3) {
+
             System.out.println(index);
+
             String found = input.substring(index + 1, index + 4);
-            System.out.println(found + " " + (index + 1) + " "+ (index + 4));
+            System.out.println(found + " " + (index + 1) + " " + (index + 4));
 
             index = input.indexOf("abc", index + 3);
             System.out.println("index after updating " + index);
