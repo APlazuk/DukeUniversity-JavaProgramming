8eca54c7d16fcd9381f35b68c6cf0a5797b25215
APlazuk <aleksandraplazuk@o2.pl>
2020-09-24 20:14:39 +0200

    week3 - CSV:Max Temperature

--- src/main/java/week3/CSVMax.java
+++ src/main/java/week3/CSVMax.java
@@ -1,70 +1,71 @@
 package week3; /**
  * Find the highest (hottest) temperature in a file of CSV weather data.
- * 
- * @author Duke Software Team 
+ *
+ * @author Duke Software Team
  */
+
 import edu.duke.*;
 import org.apache.commons.csv.*;
+
 import java.io.*;
 
 public class CSVMax {
-	public static CSVRecord hottestHourInFile(CSVParser parser) throws IOException {
-		//start with largestSoFar as nothing
-		CSVRecord largestSoFar = null;
-		//For each row (currentRow) in the CSV File
-		for (CSVRecord record : parser.getRecords()) {
-			//If largestSoFar is nothing
-			if (largestSoFar == null){
-				largestSoFar = record;
-			}else {
-				double currentTemp = Double.parseDouble(record.get("TemperatureF"));
-				double largestTemp = Double.parseDouble(largestSoFar.get("TemperatureF"));
-
-				if (currentTemp > largestTemp){
-					//If so update largestSoFar to currentRow
-					largestSoFar = record;
-				}
-			}
-		}
-		//The largestSoFar is the answer
-		return largestSoFar;
-	}
-	public static CSVRecord hottestInManyDays() throws IOException {
-		CSVRecord largestSoFar = null;
-		DirectoryResource dr = new DirectoryResource();
-		// iterate over files
-		for (File file : dr.selectedFiles()) {
-			FileResource fr = new FileResource(file);
-			// use method to get largest in file.}
-			CSVRecord record = hottestHourInFile(fr.getCSVParser());
-			if (largestSoFar == null){
-				largestSoFar = record;
-			}
-			double largestTemp = Double.parseDouble(largestSoFar.get("TemperatureF"));
+	public static CSVRecord getLargestOfTwo(CSVRecord largestSoFar, CSVRecord record) {
+		if (largestSoFar == null) {
+			largestSoFar = record;
+		} else {
 			double currentTemp = Double.parseDouble(record.get("TemperatureF"));
-			if (currentTemp > largestTemp){
+			double largestTemp = Double.parseDouble(largestSoFar.get("TemperatureF"));
+
+			if (currentTemp > largestTemp) {
+				//If so update largestSoFar to currentRow
 				largestSoFar = record;
 			}
 		}
 		return largestSoFar;
 	}
 
+    public static CSVRecord hottestHourInFile(CSVParser parser) throws IOException {
+        //start with largestSoFar as nothing
+        CSVRecord largestSoFar = null;
+        //For each row (currentRow) in the CSV File
+        for (CSVRecord record : parser.getRecords()) {
+            //If largestSoFar is nothing
+            largestSoFar = getLargestOfTwo(largestSoFar, record);
+        }
+        //The largestSoFar is the answer
+        return largestSoFar;
+    }
 
-	public static void testHottestInDay () throws IOException {
-		FileResource fr = new FileResource("src/main/resources/data/2015/weather-2015-01-01.csv");
-		CSVRecord largest = hottestHourInFile(fr.getCSVParser());
-		System.out.println("hottest temperature was " + largest.get("TemperatureF") +
-				   " at " + largest.get("TimeEST"));
-	}
+    public static CSVRecord hottestInManyDays() throws IOException {
+        CSVRecord largestSoFar = null;
+        DirectoryResource dr = new DirectoryResource();
+        // iterate over files
+        for (File file : dr.selectedFiles()) {
+            FileResource fr = new FileResource(file);
+            // use method to get largest in file.}
+            CSVRecord record = hottestHourInFile(fr.getCSVParser());
+            largestSoFar = getLargestOfTwo(largestSoFar, record);
+        }
+        return largestSoFar;
+    }
 
-	public static void testHottestInManyDays() throws IOException {
-		CSVRecord largest = hottestInManyDays();
-		System.out.println("hottest temperature was " + largest.get("TemperatureF") +
-				" at " + largest.get("DateUTC"));
-	}
 
-	public static void main(String[] args) throws IOException {
+    public static void testHottestInDay() throws IOException {
+        FileResource fr = new FileResource("src/main/resources/data/2015/weather-2015-01-01.csv");
+        CSVRecord largest = hottestHourInFile(fr.getCSVParser());
+        System.out.println("hottest temperature was " + largest.get("TemperatureF") +
+                " at " + largest.get("TimeEST"));
+    }
+
+    public static void testHottestInManyDays() throws IOException {
+        CSVRecord largest = hottestInManyDays();
+        System.out.println("hottest temperature was " + largest.get("TemperatureF") +
+                " at " + largest.get("DateUTC"));
+    }
+
+    public static void main(String[] args) throws IOException {
 //		testHottestInDay();
-	testHottestInManyDays();
-	}
+        testHottestInManyDays();
+    }
 }
